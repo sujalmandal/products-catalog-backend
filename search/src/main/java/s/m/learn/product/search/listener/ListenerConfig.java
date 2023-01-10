80f9bc4b45e8a2efb5b2f31869cea04354e2e425
Sujal Mandal <ss6sujal@gmail.com>
2023-01-11 01:05:44 +0400

    batch consumer

--- search/src/main/java/s/m/learn/product/search/listener/ListenerConfig.java
+++ search/src/main/java/s/m/learn/product/search/listener/ListenerConfig.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import s.m.learn.product.lib.common.CommonConstant;
 import s.m.learn.product.search.service.ProductSearchIndexService;
 
 @Configuration
--- search/src/main/java/s/m/learn/product/search/listener/ProductIndexUpdateListener.java
+++ search/src/main/java/s/m/learn/product/search/listener/ProductIndexUpdateListener.java
@@ -1,13 +1,20 @@
 package s.m.learn.product.search.listener;
 
+import com.solace.spring.cloud.stream.binder.messaging.SolaceBinderHeaders;
+import kotlin.Suppress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Payload;
 import s.m.learn.product.lib.model.ProductIndexUpdateRequest;
 import s.m.learn.product.search.service.ProductSearchIndexService;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
-public class ProductIndexUpdateListener implements Consumer<ProductIndexUpdateRequest> {
+public class ProductIndexUpdateListener implements Consumer<Message<List<Payload>>> {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProductIndexUpdateListener.class);
 
@@ -17,9 +24,19 @@ public class ProductIndexUpdateListener implements Consumer<ProductIndexUpdateRe
         this.productSearchIndexService = productSearchIndexService;
     }
 
+    /*
     @Override
-    public void accept(final ProductIndexUpdateRequest updateRequest) {
-        LOG.info("received update product message, productId : {}", updateRequest.getProductId());
-        productSearchIndexService.updateIndex(updateRequest);
+    public void accept(final Collection<Message<ProductIndexUpdateRequest>> updateRequest) {
+        LOG.info("received update product message, productId : {}", updateRequest);
+        //updateRequest.forEach(productSearchIndexService::updateIndex);
+    }
+    */
+    @Override
+    public void accept(final Message<List<Payload>> listMessage) {
+        LOG.info("received update product message, productId : {}", listMessage.getPayload().size());
+        List<Map<String, Object>> batchedHeaders = (List<Map<String, Object>>)listMessage.getHeaders()
+                .get(SolaceBinderHeaders.BATCHED_HEADERS);
+        List<Payload> batchedPayloads = listMessage.getPayload();
+        batchedPayloads.forEach(msg-> LOG.info("msg : {}", msg));
     }
 }
